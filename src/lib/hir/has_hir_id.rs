21e8459e154e1c090dd37ba915aaeb6322166e9f
Florian Greiner <florian.greiner.pro@gmail.com>
2021-09-11 04:07:04 +0200

    Add missing files

---
+++ src/lib/hir/has_hir_id.rs
@@ -0,0 +1,63 @@
+use crate::hir::*;
+use paste::paste;
+
+pub trait HasHirId {
+    fn get_hir_id(&self) -> HirId;
+}
+
+macro_rules! impl_direct_get_hir_id_trait {
+    ($(
+        $name:ident
+    )*) => {
+        $(
+            impl HasHirId for $name {
+                fn get_hir_id(&self) -> HirId {
+                    self.hir_id.clone()
+                }
+            }
+        )*
+    };
+}
+
+impl_direct_get_hir_id_trait!(
+    TopLevel
+    Prototype
+    FunctionDecl
+    Identifier
+    If
+    FunctionCall
+    Literal
+    NativeOperator
+);
+
+macro_rules! impl_indirect_get_hir_id_trait {
+    ($(
+        $name:ident
+    )*) => {
+        paste! {
+            $(
+                // impl $name {
+                //     fn get_terminal_hir_id(&self) -> HirId {
+                //         self.[<walk_ $name:snake>]()
+                //     }
+                // }
+                impl HasHirId for $name {
+                    fn get_hir_id(&self) -> HirId {
+                        self.get_terminal_hir_id()
+                    }
+                }
+            )*
+        }
+    };
+}
+
+impl_indirect_get_hir_id_trait!(
+    Assign
+    ArgumentDecl
+    IdentifierPath
+    FnBody
+    Body
+    Statement
+    Expression
+    Else
+);
---
+++ src/lib/hir/hir_printer.rs
@@ -0,0 +1,120 @@
+use paste::paste;
+use std::fmt::Debug;
+
+use crate::ast::{Type, TypeSignature};
+use crate::helpers::*;
+use crate::hir::visit::*;
+use crate::hir::HasHirId;
+use crate::hir::*;
+
+pub struct HirPrinter<'a> {
+    hir: &'a Root,
+    indent: usize,
+}
+impl<'a> HirPrinter<'a> {
+    pub fn new(hir: &'a Root) -> Self {
+        Self { hir, indent: 0 }
+    }
+
+    pub fn increment(&mut self) {
+        self.indent += 1;
+    }
+
+    pub fn decrement(&mut self) {
+        self.indent -= 1;
+    }
+
+    // pub fn get_token(&self, token_id: TokenId) -> Option<Token> {
+    //     self.tokens.get(token_id).cloned()
+    // }
+
+    pub fn indent(&self) -> usize {
+        self.indent
+    }
+
+    pub fn print<T: ClassName + HasHirId>(&self, t: T) {
+        let indent_str = String::from("  ").repeat(self.indent());
+
+        let t_id = self.hir.node_types.get(&t.get_hir_id());
+        let ty = match t_id {
+            Some(t_id) => self.hir.types.get(&t_id),
+            None => None,
+        };
+
+        println!(
+            "{:?}{}{:15}{:?}:{:?}",
+            t.get_hir_id(),
+            indent_str,
+            t.class_name_self(),
+            t_id,
+            ty,
+        );
+    }
+
+    pub fn print_primitive<T>(&self, t: T)
+    where
+        T: Debug,
+    {
+        let indent_str = String::from("  ").repeat(self.indent());
+
+        println!("{:9}{}{:15?}", "", indent_str, t);
+    }
+}
+
+macro_rules! impl_visitor_trait2 {
+    ($(
+        $name:ident
+    )*) => {
+        impl<'a> Visitor<'a> for HirPrinter<'a> {
+            fn visit_name(&mut self, name: String) {
+                self.print_primitive(name);
+            }
+
+            fn visit_type(&mut self, t: &Type) {
+                self.print_primitive(t);
+            }
+
+            fn visit_primitive<T>(&mut self, val: T)
+            where
+                T: Debug,
+            {
+                self.print_primitive(val);
+            }
+
+            paste! {
+
+                $(
+                    fn [<visit_ $name:snake>](&mut self, item: &'a $name) {
+                        self.print(item.clone());
+
+                        self.increment();
+
+                        [<walk_ $name:snake>](self, item);
+
+                        self.decrement();
+                    }
+                )*
+            }
+        }
+    };
+}
+
+impl_visitor_trait2!(
+    TopLevel
+    Assign
+    Prototype
+    FunctionDecl
+    ArgumentDecl
+    // IdentifierPath
+    Identifier
+    FnBody
+    Body
+    Statement
+    // Expression
+    If
+    Else
+    FunctionCall
+    Literal
+    NativeOperator
+    // TypeSignature
+);
