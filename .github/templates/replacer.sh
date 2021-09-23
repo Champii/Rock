9dec8e53e276c43a507027477ea8a2275ffe8506
Florian Greiner <florian.greiner.pro@gmail.com>
2021-09-23 04:44:27 +0200

    Trying again

--- .github/templates/replacer.sh
+++ .github/templates/replacer.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-NEW_VERSION=$(cat .github/version)-$GITHUB_REF
+# NEW_VERSION=$(cat .github/version)-$GITHUB_REF
+NEW_VERSION=test-version
 
 sed "s/{version}/$NEW_VERSION/g" "./.github/templates/README.md" > README.md
 
