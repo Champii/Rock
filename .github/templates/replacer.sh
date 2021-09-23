eecf7bcc37f3e830e78a44da2f971bdcd022070a
Florian Greiner <florian.greiner.pro@gmail.com>
2021-09-23 04:38:13 +0200

    Trying again

--- .github/templates/replacer.sh
+++ .github/templates/replacer.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-sed "s/{version}/${NEW_VERSION}/g" ./.github/templates/README.md > README.md
+NEW_VERSION=$(cat .github/version)-$GITHUB_REF
 
-sed "s/{version}/${NEW_VERSION}/g" ./.github/templates/Cargo.toml > Cargo.toml
+sed "s/{version}/${NEW_VERSION}/g" "./.github/templates/README.md" > README.md
+
+sed "s/{version}/${NEW_VERSION}/g" "./.github/templates/Cargo.toml" > Cargo.toml
