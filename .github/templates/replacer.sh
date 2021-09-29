ba8afddf6239bf8a7dc39ab419524687be3ab20a
Florian Greiner <florian.greiner.pro@gmail.com>
2021-09-29 11:12:31 +0200

    Fix readme build status

--- .github/templates/replacer.sh
+++ .github/templates/replacer.sh
@@ -2,17 +2,16 @@
 set -euo pipefail
 
 BRANCH=$(basename $GITHUB_REF)
-
-
-BRANCH=$(printf '%s' "$BRANCH" | sed -e 's/_/-/g')
-
 NEW_VERSION=$(cat .github/version)-$BRANCH
-
 REPLACE=$(printf '%s\n' "$NEW_VERSION" | sed -e 's/[\/&]/\\&/g')
+
+BRANCH_SAFE=$(printf '%s' "$BRANCH" | sed -e 's/_/-/g')
+NEW_VERSION_SAFE=$(cat .github/version)-$BRANCH_SAFE
+REPLACE_SAFE=$(printf '%s\n' "$NEW_VERSION_SAFE" | sed -e 's/[\/&]/\\&/g')
 
 sed "s/{version}/$REPLACE/g" "./.github/templates/README.md" > README.md
 sed -i "s/{branch}/$BRANCH/g" "README.md"
 
-REPLACE="${REPLACE:1}"
+REPLACE_SAFE="${REPLACE_SAFE:1}"
 
-sed "s/{version}/$REPLACE/g" "./.github/templates/Cargo.toml" > Cargo.toml
+sed "s/{version}/$REPLACE_SAFE/g" "./.github/templates/Cargo.toml" > Cargo.toml
