9ba7a687a0eab470ffd6380a26acfa638143163d
Florian Greiner <florian.greiner.pro@gmail.com>
2021-10-01 15:34:23 +0200

    Separated struct type into its own module

--- src/lib/ast/type.rs
+++ src/lib/ast/type.rs
@@ -1,8 +1,10 @@
-use colored::*;
-use std::{collections::BTreeMap, fmt};
+use std::fmt;
 
-use super::FuncType;
+use colored::*;
+
 use crate::{ast::PrimitiveType, hir};
+
+use super::{FuncType, StructType};
 
 #[derive(Clone, Eq, Serialize, Deserialize)]
 pub enum Type {
@@ -224,82 +226,6 @@ impl From<String> for Type {
             Type::Trait(t)
         }
     }
-}
-
-#[derive(Clone, Hash, PartialEq, Eq, Serialize, Deserialize)]
-pub struct StructType {
-    pub name: String,
-    pub defs: BTreeMap<String, Box<Type>>,
-}
-
-impl fmt::Debug for StructType {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(
-            f,
-            "{} {} {} {}",
-            self.name.yellow(),
-            "{".green(),
-            self.defs
-                .iter()
-                .map(|(n, b)| format!("{}: {:?}", n, b))
-                .collect::<Vec<_>>()
-                .join(", "),
-            "}".green(),
-        )
-    }
-}
-
-impl From<&super::StructDecl> for StructType {
-    fn from(s: &super::StructDecl) -> Self {
-        s.into()
-    }
-}
-impl From<super::StructDecl> for StructType {
-    fn from(s: super::StructDecl) -> Self {
-        StructType {
-            name: s.name.get_name(),
-            defs: s
-                .defs
-                .iter()
-                .map(|proto| {
-                    if proto.signature.arguments.is_empty() {
-                        (proto.name.name.clone(), proto.signature.ret.clone())
-                    } else {
-                        (
-                            proto.name.name.clone(),
-                            Box::new(proto.signature.clone().into()),
-                        )
-                    }
-                })
-                .collect(),
-        }
-    }
-}
-
-impl From<hir::StructDecl> for StructType {
-    fn from(s: hir::StructDecl) -> Self {
-        StructType {
-            name: s.name.get_name(),
-            defs: s
-                .defs
-                .iter()
-                .map(|proto| {
-                    if proto.signature.arguments.is_empty() {
-                        (proto.name.name.clone(), proto.signature.ret.clone())
-                    } else {
-                        (
-                            proto.name.name.clone(),
-                            Box::new(proto.signature.clone().into()),
-                        )
-                    }
-                })
-                .collect(),
-        }
-    }
-}
-
-impl StructType {
-    // pub fn
 }
 
 #[cfg(test)]
